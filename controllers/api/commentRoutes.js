16851a96700ff127c1937416aaea6aa2415520fc
Israa Beseiso <israabeseiso@gmail.com>
2021-03-09 08:11:07 -0700

    updated comment route

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const {Comment} = require('../../models');
+const withAuth = require('../../utils/auth')
+
+router.post('/', withAuth, (req, res)=>{
+  Comment.create({...req.body, userId: req.session.userId})
+  .then(comment=>{
+    res.json(comment)
+  }).catch(err=>{
+    res.status(500).json(err)
+  })
+})
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes.js');
 const postRoutes = require('./projectRoutes.js');
+const commentRoutes = require('./commentRoutes')
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comment', commentRoutes)
 
 module.exports = router;
